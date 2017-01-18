7890b31e257e8b29cea9beb4f990aeb466e5b208
Dom Wakeling <domwakeling@gmail.com>
2017-01-18 22:16:52 +0000

    Map fixed, adding other styling

--- slopes_js.js
+++ slopes_js.js
@@ -6,22 +6,19 @@ var margin = {
 };
 
 var mouseOrig;
-//var anglesOrig = [-6, -54.25, 3]; //[3, -54.25, -3]
-var anglesCurr = [3, -54.25, -2];
-//var anglesOrig = [0,0];
-//var anglesCurr = [0,0];
-var zoomCurr = 4000;
-//var zoomMin = 30;
-//var zoomMax = 2500;
-//var mSc = 8;  mouse scale for rotations
-//var zoomFac = 150; // factor for zoom sensitivity (higher = less sensitive)
+var anglesOrig = [4.5, -54.65, -2]; //[3, -54.25, -3]
+var anglesCurr = [4.5, -54.65, -2];
+var zoomCurr = 3250;
+var zoomMin = 3250;
+var zoomMax = 10000;
+var mSc = 20;  //mouse scale for rotations
+var zoomFac = 200; // factor for zoom sensitivity (higher = less sensitive)
 
 var width = 450 - margin.left - margin.right; // base number needs to match wrapper
-var height = 650 - margin.top - margin.bottom;
+var height = 550 - margin.top - margin.bottom;
 
-var projection = d3.geoOrthographic()
-//var projection = d3.geoConicEqualArea()
-//var projection = d3.geoMercator()
+//var projection = d3.geoOrthographic()
+var projection = d3.geoMercator()
     .scale(zoomCurr)
     .clipAngle(90)
     .translate([width / 2, height / 2])
@@ -46,8 +43,7 @@ var div = d3.select("body").append("div")
     .style("opacity", 0);
 
 // URLs for [1] a 110-m map stored in a GitHub repo and [2] the slopes data in a GitHub repo
-//var url1 = "https://raw.githubusercontent.com/domwakeling/UK-ski-slopes-map/master/uk110m.json";
-var url1 = "https://raw.githubusercontent.com/domwakeling/UK-ski-slopes-map/master/british-isles_geo.json"
+var url1 = "https://raw.githubusercontent.com/domwakeling/UK-ski-slopes-map/master/world.json"
 var url2 = "https://raw.githubusercontent.com/domwakeling/UK-ski-slopes-map/master/skislopes.json";
 
 d3.queue(2)
@@ -59,23 +55,19 @@ d3.queue(2)
     });
 
 function renderMap(topology, slopes) {
-	console.log(topology.features);
     g.selectAll("path.country")
-//        .data(topojson.feature(topology, topology.objects.countries).features)
-//		.data(topojson.feature(topology, topology).features.geometry)
 		.data(topology.features)
         .enter()
         .append("path")
         .attr("class", "country")
         .attr("d", path);
 
-//    chart.on("mousedown", mouseDown)
-//        .on("mousewheel", zoomed);
-//	chart.on("mousewheel", zoomed);
-//
-//    d3.select(window)
-//        .on("mousemove", mouseMoved)
-//        .on("mouseup", mouseUp);
+    chart.on("mousedown", mouseDown)
+        .on("mousewheel", zoomed);
+
+    d3.select(window)
+        .on("mousemove", mouseMoved)
+        .on("mouseup", mouseUp);
 
     renderslopes(slopes);
 }
@@ -93,14 +85,12 @@ function renderslopes(slopes) {
         .datum(function(d) {
             var slopeObj = circle.radius(0.07).center(d.geometry.coordinates)();
             slopeObj.name = d.properties.name;
-//            slopeObj.slopeURL = d.properties.slopeURL;
             return slopeObj;
         })
         .attr("class", "slopes")
         .attr("d", path)
         .on("mouseover", function(d) {
 	        var str = d.name;
-//            var str = d.name + "<br />" + d.slopeURL;
             div.transition()
                 .duration(200)
                 .style("opacity", 1.0);
@@ -115,36 +105,35 @@ function renderslopes(slopes) {
 }
 
 
-//function zoomed() {
-//    zoomCurr *= (zoomFac + d3.event.deltaY) / zoomFac;
-//    zoomCurr = Math.min(zoomMax, Math.max(zoomMin, zoomCurr));
-//    projection.scale(zoomCurr);
-//    refresh();
-//    d3.event.preventDefault;
-//}
-//
-//function mouseDown() {
-//    mouseOrig = [d3.event.pageX, d3.event.pageY];
-//    console.log("orig:", anglesOrig, "curr:", anglesCurr)
-//    anglesOrig = [anglesCurr[0], anglesCurr[1]];
-//    d3.event.preventDefault();
-//}
-//
-//function mouseMoved() {
-//    if (mouseOrig) {
-//        var mouseCurr = [d3.event.pageX, d3.event.pageY];
-//        anglesCurr = [anglesOrig[0] - (mouseOrig[0] - mouseCurr[0]) / mSc, anglesOrig[1] - (mouseCurr[1] - mouseOrig[1]) / mSc];
-//        projection.rotate(anglesCurr);
-//        refresh();
-//    }
-//}
-//
-//function mouseUp() {
-//    if (mouseOrig) {
-//        mouseMoved();
-//        mouseOrig = null;
-//    }
-//}
+function zoomed() {
+	d3.event.preventDefault();
+    zoomCurr *= (zoomFac + d3.event.deltaY) / zoomFac;
+    zoomCurr = Math.min(zoomMax, Math.max(zoomMin, zoomCurr));
+    projection.scale(zoomCurr);
+    refresh();
+}
+
+function mouseDown() {
+    mouseOrig = [d3.event.pageX, d3.event.pageY];
+    anglesOrig = [anglesCurr[0], anglesCurr[1], anglesCurr[2]];
+    d3.event.preventDefault();
+}
+
+function mouseMoved() {
+    if (mouseOrig) {
+        var mouseCurr = [d3.event.pageX, d3.event.pageY];
+        anglesCurr = [anglesOrig[0] - (mouseOrig[0] - mouseCurr[0]) / mSc, anglesOrig[1] - (mouseCurr[1] - mouseOrig[1]) / mSc, anglesOrig[2]];
+        projection.rotate(anglesCurr);
+        refresh();
+    }
+}
+
+function mouseUp() {
+    if (mouseOrig) {
+        mouseMoved();
+        mouseOrig = null;
+    }
+}
 
 function refresh() {
     chart.selectAll("path").attr("d", path);
